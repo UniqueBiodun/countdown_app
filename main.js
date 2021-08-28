b032a89243d70ffac72056b9990380b702efbea1
UniqueBiodun <aduabiodunemmanuel@gmail.com>
2021-08-28 13:31:14 +0100

    Countdown styles modified and some js lines commented

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let initialCountdownVal = countdownElement.innerHTML;
 let interval = setInterval(() => {
     initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal - 1 : 0;
     countdownElement.innerHTML = initialCountdownVal;
-    countdownElement.style.fontSize = initialCountdownVal * 100 + "px";
+    // countdownElement.style.fontSize = initialCountdownVal * 100 + "px";
 
-    console.log(initialCountdownVal * 100 + 'px');
+    // console.log(initialCountdownVal * 100 + 'px');
 
     if (initialCountdownVal <= 0) {
         clearInterval(interval);
